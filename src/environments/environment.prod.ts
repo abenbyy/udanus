464f385877b1f44b1c317be5cecaa98f5f674596
abenbyy <andrbenaya@gmail.com>
2020-05-11 17:31:32 +0700

    Hotfix #2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB2-WjFDiruRzBPKrYdXmxn-sHUD3YXPcI",
+    authDomain: "udanus-e7b26.firebaseapp.com",
+    databaseURL: "https://udanus-e7b26.firebaseio.com",
+    projectId: "udanus-e7b26",
+    storageBucket: "udanus-e7b26.appspot.com",
+    messagingSenderId: "634556267220",
+    appId: "1:634556267220:web:13f5703a38ce952239d941",
+    measurementId: "G-YFQHGJZRD6",
+  }
 };
