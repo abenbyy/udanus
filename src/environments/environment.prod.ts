5de9571b1e6d8cd9b62ff93414675d1f3555ea80
abenbyy <andrbenaya@gmail.com>
2020-05-15 23:24:26 +0700

    Hotfix #3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB2-WjFDiruRzBPKrYdXmxn-sHUD3YXPcI",
-    authDomain: "udanus-e7b26.firebaseapp.com",
-    databaseURL: "https://udanus-e7b26.firebaseio.com",
-    projectId: "udanus-e7b26",
-    storageBucket: "udanus-e7b26.appspot.com",
-    messagingSenderId: "634556267220",
-    appId: "1:634556267220:web:13f5703a38ce952239d941",
-    measurementId: "G-YFQHGJZRD6",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
   }
 };
